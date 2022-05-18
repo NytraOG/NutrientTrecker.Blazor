84ea5a899c7bc5e70d6cab6c78db5c2aa1ae110e
Christian Nytra <christian.nytra@co-orga.d>
2022-05-18 20:10:43 +0200

    Init

---
+++ Frontend/FileUtils.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutrientTrecker
+{
+    public static class FileUtils
+    {        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+       => js.InvokeAsync<object>(
+           "saveAsFile",
+           filename,
+           Convert.ToBase64String(data));
+    }
+}
---
+++ Frontend/Program.cs
@@ -0,0 +1,38 @@
+﻿using NutrientTrecker.Data;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddSingleton<PdfService>();
+builder.Services.AddSingleton<ExcelService>();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorPages();
+
+builder.Services.AddServerSideBlazor()
+       .AddHubOptions(o =>
+        {
+            o.MaximumReceiveMessageSize = 102400000;
+        });
+
+var app = builder.Build();
+
+SyncfusionLicenseProvider.RegisterLicense(app.Configuration.GetValue<string>("LicenseKey"));
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
